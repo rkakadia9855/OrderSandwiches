05143977d549613872f8ec4d57e5dea0d958ee3b
rkakadia9855 <rudrakakadia9855@gmail.com>
2020-11-15 19:10:37 +0530

    set the selectionmodel of listview to multiple

--- src/application/OrderSummaryController.java
+++ src/application/OrderSummaryController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -154,7 +155,8 @@ public class OrderSummaryController implements Initializable{
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+
+	    receipt.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		
 	}
 
